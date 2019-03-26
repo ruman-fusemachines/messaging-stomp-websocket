707fe44fcda397e1d713d474cf011aee4392ea91
ruman <ruman@fusemachines.com>
2019-03-26 10:30:02 +0545

    initializing project for stomp

---
+++ src/main/java/com/example/stomp/MessagingStompWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingStompWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
+	}
+
+}
